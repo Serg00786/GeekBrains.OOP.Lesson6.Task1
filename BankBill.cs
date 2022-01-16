f44870be78cb247686ea1d21ece4e0446823166e
Serg <a_s_v86@mail.ru>
2022-01-16 08:41:38 +0300

    Initial

---
+++ BankBill.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GeekBrains.OOP.Lesson2.Task1
+{
+    class BankBill
+    {
+        internal long BillNumber {get; set; }
+        internal double Balance {get; set; }
+
+        internal enum BillTypes{
+             CompanyBill,
+             PrivateBill
+        }
+        private BillTypes _BillTypes;
+
+        internal BillTypes CurrentBillTypes
+        {
+            get { return _BillTypes; }
+            set { _BillTypes = value; }
+        }
+
+        internal double FromDeposite(double IncomingSum)
+        {
+            if (Balance > IncomingSum)
+            {
+
+                return Balance-IncomingSum;
+            }
+            else
+            {
+                return Balance;
+            }
+        }
+
+        internal double ToDeposite(double IncomingSum)
+        {
+            return Balance+IncomingSum;
+        }
+
+        internal string Transaction(BankBill bankBill, int sum)
+        {
+            return "Transferred succesfully";
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GeekBrains.OOP.Lesson2.Task1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            BankBill bankBill = new BankBill();
+            bankBill.Balance = 1111;
+            bankBill.BillNumber = 22222;
+            bankBill.CurrentBillTypes = BankBill.BillTypes.CompanyBill;
+            
+            var balance = bankBill.FromDeposite(11);
+            Console.WriteLine(balance);
+
+            balance = bankBill.ToDeposite(1100);
+            Console.WriteLine(balance);
+
+            int sum = 1000;
+
+            if (balance > sum)
+            {
+               string result= bankBill.Transaction(bankBill, sum);
+                Console.WriteLine(result);
+            }
+
+
+
+
+            Console.ReadKey();
+        }
+    }
+}
